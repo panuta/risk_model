fe36aed654887a36cd1c8df9bace93404b00af08
Panu Tangchalermkul <panuta@gmail.com>
2018-02-12 17:24:10 +0700

    Render form by type of model field

--- client/src/components/ModelObjectForm.vue
+++ client/src/components/ModelObjectForm.vue
@@ -3,19 +3,32 @@
     <h3>New {{ riskModel.name }} Form</h3>
 
     <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
+      <template class="form-group" v-for="field in riskModel.fields">
+        <div class="form-group">
+          <label :for="'id_' + field.slug">{{ field.name }}</label>
+
+          <template v-if="field.type === 'text'">
+            <input type="text" class="form-control" :id="'id_' + field.slug" :name="field.slug">
+          </template>
+
+          <template v-if="field.type === 'number'">
+            <input type="number" class="form-control" :id="'id_' + field.slug" :name="field.slug">
+          </template>
+
+          <template v-if="field.type === 'date'">
+            <datepicker bootstrap-styling="true"/>
+          </template>
+
+          <template v-if="field.type === 'enum'">
+            <select class="form-control" :id="'id_' + field.slug" :name="field.slug">
+              <option></option>
+              <option v-for="choice in field.choices.split(',')" :value="choice">{{ choice }}</option>
+            </select>
+          </template>
+
+          <small v-if="!field.is_required" class="form-text text-muted">This field is optional</small>
+        </div>
+      </template>
 
       <div class="form-buttons">
         <button type="submit" class="btn btn-primary">Submit</button>
@@ -25,8 +38,13 @@
 </template>
 
 <script>
+  import Datepicker from 'vuejs-datepicker';
+
   export default {
     name: 'model-object-form',
+    components: {
+        Datepicker
+    },
     computed: {
       riskModel () {
         const model_uuid = this.$route.params.uuid;
@@ -35,6 +53,12 @@
     }
   }
 </script>
+
+<style>
+  .form-control[readonly] {
+    background-color: #fff !important;
+  }
+</style>
 
 <style scoped>
   .container {
@@ -45,5 +69,13 @@
     font-size: 1.6rem;
     font-weight: bold;
     margin-bottom: 1rem;
+  }
+
+  .form-buttons {
+    margin-top: 1.5rem;
+  }
+
+  .form-buttons .btn {
+    padding: .5rem 2.3rem;
   }
 </style>
